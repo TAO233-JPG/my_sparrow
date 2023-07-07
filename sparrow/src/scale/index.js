f250881beb4485b73402873d195b0ad56c797081
TAO233-JPG <1040581451@qq.com>
2023-07-07 20:13:21 +0800

    feat: 统一导出比例尺

---
+++ sparrow/src/scale/index.js
@@ -0,0 +1,21 @@
+import createQuantize from "./quantize";
+import createThreshold from "./threshold";
+import createQuantile from "./quantile";
+import createIdentity from "./identity";
+import createLinear from "./linear";
+import createTime from "./time";
+import createOrdinal from "./ordinal";
+import createBand from "./band";
+import createPoint from "./point";
+
+export default {
+  createBand,
+  createIdentity,
+  createLinear,
+  createOrdinal,
+  createPoint,
+  createQuantile,
+  createQuantize,
+  createTime,
+  createThreshold,
+};
--- sparrow/src/scale/quantile.js
+++ sparrow/src/scale/quantile.js
@@ -1,6 +1,6 @@
 import createThreshold from "./threshold";
 
-export function createQuantile({ domain, range, ...rest }) {
+export default function createQuantile({ domain, range, ...rest }) {
   const n = range.length - 1;
   const sortedDomain = domain.sort((a, b) => a - b);
   const step = (sortedDomain.length - 1) / (n + 1);
