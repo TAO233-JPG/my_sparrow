a8ef05af1f97817d62fd308c02d929ca864d2eb6
TAO233-JPG <1040581451@qq.com>
2023-07-09 18:01:25 +0800

    feat: 统一导出gemetry相关函数

---
+++ sparrow/src/geometry/index.js
@@ -0,0 +1,6 @@
+import { text } from "./text";
+import { point } from "./point";
+import { createGemetry } from "./geometry";
+import { createChannel, createChannels } from "./channel";
+
+export { text, point, createGemetry, createChannel, createChannels };
